a07833176b4b3eed85b2a1a44725ddd133810cbb
J S Rajeev Sheshank <63466665+RajeevSheshank@users.noreply.github.com>
2025-02-18 09:55:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoRoom from './VideoRoom';
+import './styles.css';
+
+function app() {
+    return (
+        <div className="App">
+            <VideoRoom />
+        </div>
+    );
+}
+
+export default app;
